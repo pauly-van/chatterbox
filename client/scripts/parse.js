f16769868f668706f9800d1f3bab512fa94cbb20
Paul Nguyen <paulngu1120@gmail.com>
2020-09-28 15:51:49 +0000

    Messing around with the project before pairing

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,7 +3,16 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function(message, successCB, errorCB = null) {
-    // todo: save a message to the server
+    $.ajax({
+      url: server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: successCB,
+      error: errorCB || function(error){
+        console.log('chatterbox: Failed to send messages', data);
+      }
+    });
   },
 
   readAll: function(successCB, errorCB = null) {
